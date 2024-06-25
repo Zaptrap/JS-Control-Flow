bdf00027bbe9937677f66c757b7bc83215e1e6a9
JPesante <jpesante24@gmail.com>
2024-06-24 21:13:18 -0400

    newPlacement

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('js:loaded')
+// control is the way of how we write code to change/manage the order of which he code runs
+// three constructs - approach/ snytaxes for managing control flow
+
+// sequencing - when the browser execute the code in our js file, it reads the file through the file, top to bottom, then run from top to bottom
+// Looping: The code in the loop repeatedly executes while a condition is truthy (run code for number of times [n])
+// Branching: Different code paths are executed based on a conditional expression (either or / if else statments)
+
+let color = "maroon";
+//let color = prompt[]
+
+if (color === green) {
+    console.log('Go');
+}   else if (color === "yellow") {
+        console.log ("Slow");
+}   else if (color === "red") {
+        console.log("Stop");
+}   else {
+        console.log("Whatever");
+}
